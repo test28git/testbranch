caf90754f1b039453f0699ce6b6d8c5434733cb7
docent28 <eaa67@mail.ru>
2021-09-22 15:46:53 +0300

    Initial branch

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text("PRIVET"),
+      body: Text("PRIVET001"),
     );
   }
 }
