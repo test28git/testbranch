a27d9e8cacd12c2ebf64398b2c02669a421a199e
docent28 <eaa67@mail.ru>
2021-09-22 14:34:45 +0300

    Initial template

---
+++ android/app/src/main/kotlin/ru/home/lesson_001/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.home.lesson_001
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("PRIVET"),
+    );
+  }
+}
